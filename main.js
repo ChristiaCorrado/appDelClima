f999a327ede097494b3ddd53b56042063ce7d9af
ChristiaCorrado <86751141+ChristiaCorrado@users.noreply.github.com>
2022-01-21 19:03:12 -0300

    actualizacionApp

--- main.js
+++ main.js
@@ -1,49 +1,19 @@
 
 //URLBASE: api.openweathermap.org/data/2.5/weather?
-const APIKEY = '051c9b2018fbb89f25ac025af384e5c9';
 
-const getData = position => {
-    const {latitude, longitude} = position.coords;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${APIKEY}`)
+//api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
+
+
+const dataCity = () => {
+   input = document.getElementById("dataInput").value
+   console.log(input);
+   cityName = input
+   
+   console.log(cityName);
+   
+  
+    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${APIKEY}&lang=${lang}`)
     .then(response => response.json())
-    .then(data => crearDatosaPintar(data))
-
+    .then(data => mostrarClima(data))
 }
 
-const crearDatosaPintar = data => {
-    console.log(data)
-     const climaData = {
-            location: data.name,
-            temperature: data.main.temp,
-            feels: data.main.feels_like,
-            humidity: data.main.humidity,
-            pressure: data.main.pressure,
-            tempMax: data.main.temp_max,
-            tempMin: data.main.temp_min,
-            description: data.weather[0].description,
-        }
-    
-        Object.keys(climaData).forEach(key => {
-            document.getElementById(key).textContent = climaData[key]
-        })
-}
-
-
-const onload = () => {
-    navigator.geolocation.getCurrentPosition(getData)
-}
-
-const getDataInput = () => {
-    let dataI = document.getElementById('dataInput').value
-    console.log(dataI);
-
-    getClimaInput(dataI)
-
-}
-
-const getClimaInput = dataI => {
-    const cityName = dataI;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=units=metric&${cityName}&appid=${APIKEY}`)
-    .then(response => console.log(response))
-    
-}
--- main2.js
+++ main2.js
@@ -3,12 +3,14 @@ const obtenerCoord = () => {
      
 }
 
+let lang = 'es'
+
 const coordinadas = (coord) => {
    let arrayCoord = coord.coords
    lat = arrayCoord.latitude
    long = arrayCoord.longitude
 
-   fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${long}&appid=${APIKEY}`)
+   fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${long}&appid=${APIKEY}&lang=${lang}`)
     .then((response) => response = response.json())
     .then(data => mostrarClima(data))
 }
@@ -27,8 +29,8 @@ const mostrarClima = dataAPI => {
 
     const Clima = {
         location: dataAPI.name,
-        temperature: dataAPI.main.temp,
-        sensacion: dataAPI.main.feels_like,
+        temperature: `${Math.round(dataAPI.main.temp)}ºC` ,
+        sensacion: `${Math.round(dataAPI.main.feels_like)}ºC`,
         humedad: dataAPI.main.humidity,
         precion: dataAPI.main.pressure,
         tempMax: dataAPI.main.temp_max,
@@ -37,12 +39,45 @@ const mostrarClima = dataAPI => {
     }
 
     Object.keys(Clima).forEach(key => {
-        document.getElementById(key).innerHTML = Clima[key]
+        document.getElementById(key).innerHTML = `${Clima[key]}`
     })
+
     
+    const selectFondo = document.getElementById('body')
+    console.log(selectFondo);
+    const tipoDeClima = Clima.descripcion
+    console.log(tipoDeClima);
+
+    switch (tipoDeClima) {
+        
+        case 'cielo claro':
+            selectFondo.classList.add('dayClear')
+            selectFondo.classList.remove('nightMist','nubesDispersas','algoDeNubes','nubes','niebla','dayRain');
+            break;
+        case 'nubes dispersas':
+            selectFondo.classList.add('nubesDispersas')
+            selectFondo.classList.remove('nightMist','dayClear','algoDeNubes','nubes','niebla','dayRain');
+            break;
+        case 'nubes','algo de nubes':
+            selectFondo.classList.add('algoDeNubes')
+            selectFondo.classList.remove('nightMist','dayClear','nubesDispersas','nubes','niebla','dayRain');
+            break;
+        case 'muy nuboso':
+            selectFondo.classList.add('nubes')
+            selectFondo.classList.remove('nightMist','dayClear','nubesDispersas','algoDeNubes','niebla','dayRain');
+            break;
+        case 'niebla':
+            selectFondo.classList.add('niebla')
+            selectFondo.classList.remove('nightMist','dayClear','nubesDispersas','algoDeNubes','nubes','dayRain');
+            break; 
+        case 'tormenta con lluvia ligera','lluvia moderada','tormenta con lluvia':
+            selectFondo.classList.add('dayRain') 
+            selectFondo.classList.remove('nightMist','dayClear','nubesDispersas','algoDeNubes','nubes','niebla');
+            break;
+    }
 }
 
-let hora = new Date();
+let hora = new Date()
 
 const horaEnVivo = () => {
     let horaLocal = `${hora.getHours()}:${hora.getMinutes()}:${hora.getSeconds()}`;
@@ -52,3 +87,4 @@ const horaEnVivo = () => {
     setTimeout("hora",1000)
 
 }
+
